5e7e605fc0624ac770180ff53c19acc44775f72f
ShreyaCV <105869425+ShreyaCV@users.noreply.github.com>
2022-05-19 19:56:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function spanelement() {
+    console.log("It is a span tag.")
+    document.getElementById("song1").play()
+}
+
+function divelement() {
+    console.log("It is a div tag.")
+    document.getElementById("song2").play()
+}
